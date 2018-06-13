5a2bdf16fd1e2521eac8da6fa3168a5b596b2af4
Javier Richards Valenzuela <jrichards@udec.cl>
2018-06-13 02:14:04 -0400

    matrices de transicion al parecer esta bien

--- viterbi.py
+++ viterbi.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 
 def create_map():
@@ -36,21 +37,17 @@ def create_map():
     mapa[3][11]=1
     mapa_aux=[]
     for i in mapa:
-        print(i)
         aux=[1]
         i=aux+i
         i.append(1)
         mapa_aux.append(i)
-    print("---")
+
     
     a=[[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
     
     mapa_aux=a+mapa_aux+a
-    for i in mapa_aux:
-        print(i)
-    print("---")
 
-    return mapa
+    return mapa_aux
 
 def init_secuence(i):
     #Secuencia observable
@@ -65,59 +62,127 @@ def init_secuence(i):
     sequence.append(E4)
 
     return sequence[i-1]
+def states_prob(mapa):
+    states=[]
 
-def emision(sequence,mapa):
+    for i,m in enumerate(mapa):
+        states.append([])
+        for j in m:
+            if(j==1):
+                states[i].append(0)
+            else:
+                states[i].append(1/44)
+    return states
+
+
+def emision(mapa,sequence):
     m_emision = []
 
-    for i in sequence:
+   
+    for s,i in enumerate(sequence):
         m_emision.append([])
-        for j in mapa:
-            for k in j:             #sur               #norte                 #este               #oeste
+        for j in range(1,5):
+            for k in range(1,17):
+
+                                   #sur               #norte                 #este               #oeste
                 if(i=="NS"  and mapa[j+1][k]==1 and mapa[j-1][k]==1 and mapa[j][k-1]==0 and mapa[j][k+1]==0):
-                    m_emision.append(700/64)
-                if(i=="NWE" and mapa[j+1][k]==0 and mapa[j-1][k]==1 and mapa[j][k-1]==1 and mapa[j][k+1]==1):
-                    m_emission.append(200/64)
-                if(i=="NSE" and mapa[j+1][k]==1 and mapa[j-1][k]==1 and mapa[j][k-1]==1 and mapa[j][k+1]==0):
-                    m_emission.append(200/64)
-                if(i=="WE"  and mapa[j+1][k]==0 and mapa[j-1][k]==0 and mapa[j][k-1]==1 and mapa[j][k+1]==1):
-                    m_emission.append(400/64)
-                if(i=="NSW"  and mapa[j+1][k]==1 and mapa[j-1][k]==1 and mapa[j][k-1]==0 and mapa[j][k+1]==1):
-                    m_emission.append(200/64) 
-                if(i=="E"  and mapa[j+1][k]==0 and mapa[j-1][k]==0 and mapa[j][k-1]==1 and mapa[j][k+1]==0):
-                    m_emission.append(200/64)    
+                    m_emision[s].append(700/64)
+                elif(i=="NWE" and mapa[j+1][k]==0 and mapa[j-1][k]==1 and mapa[j][k-1]==1 and mapa[j][k+1]==1):
+                    m_emision[s].append(200/64)
+                elif(i=="NSE" and mapa[j+1][k]==1 and mapa[j-1][k]==1 and mapa[j][k-1]==1 and mapa[j][k+1]==0):
+                    m_emision[s].append(200/64)
+                elif(i=="WE"  and mapa[j+1][k]==0 and mapa[j-1][k]==0 and mapa[j][k-1]==1 and mapa[j][k+1]==1):
+                    m_emision[s].append(400/64)
+                elif(i=="NSW"  and mapa[j+1][k]==1 and mapa[j-1][k]==1 and mapa[j][k-1]==0 and mapa[j][k+1]==1):
+                    m_emision[s].append(200/64) 
+                elif(i=="E"  and mapa[j+1][k]==0 and mapa[j-1][k]==0 and mapa[j][k-1]==1 and mapa[j][k+1]==0):
+                    m_emision[s].append(200/64)
+                else:
+                    m_emision[s].append(0)
+
+    return m_emision  
+
+def init_states(mapa):
+    states=[]
+
+    for i,r in enumerate(mapa):                
+        for j,c in enumerate(r):            
+            if(mapa[i][j]==0):
+                states.append([i,j])
+    return states
 
 
 
 
 
-def viterbi(hmm, initial_dist, emissions):
-    probs = hmm.emission_dist(emissions[0]) * initial_dist
-    stack = []
+def viterbi(obs_seq):
+        # returns the most likely state sequence given observed sequence x
+        # using the Viterbi algorithm
+        T = len(obs_seq)
+        N = A.shape[0]
+        delta = np.zeros((T, N))
+        psi = np.zeros((T, N))
+        delta[0] = pi*B[:,obs_seq[0]]
+        for t in range(1, T):
+            for j in range(N):
+                delta[t,j] = np.max(delta[t-1]*A[:,j]) * B[j, obs_seq[t]]
+                psi[t,j] = np.argmax(delta[t-1]*A[:,j])
 
-    for emission in emissions[1:]:
-        trans_probs = hmm.transition_probs * np.row_stack(probs)
-        max_col_ixs = np.argmax(trans_probs, axis=0)
-        probs = hmm.emission_dist(emission) * trans_probs[max_col_ixs, np.arange(hmm.num_states)]
+        # backtrack
+        states = np.zeros(T, dtype=np.int32)
+        states[T-1] = np.argmax(delta[T-1])
+        for t in range(T-2, -1, -1):
+            states[t] = psi[t+1, states[t+1]]
+        return states
 
-        stack.append(max_col_ixs)
-
-    state_seq = [np.argmax(probs)]
-
-    while stack:
-        max_col_ixs = stack.pop()
-        state_seq.append(max_col_ixs[state_seq[-1]])
-
-    state_seq.reverse()
-
-    return state_seq
+def trans_matrix(mapa,states):
+    matrices=[] 
 
 
 
+    for n,i in enumerate(states):
+        matrix=[]
+        count=0
+        for r in range(6):
+            matrix.append([])
+            for j in range (18):
+                matrix[r].append(0)
+
+        if(mapa[i[0]+1][i[1]]==1):
+                count+=1                
+        else:
+            matrix[i[0]+1][i[1]]=0.25
+        if(mapa[i[0]-1][i[1]]==1):
+                count+=1
+        else:
+            matrix[i[0]-1][i[1]]=0.25
+        if(mapa[i[0]][i[1]+1]==1):
+                count+=1
+        else:
+            matrix[i[0]][i[1]+1]=0.25
+        if(mapa[i[0]][i[1]-1]==1):
+                count+=1
+        else:
+            matrix[i[0]][i[1]-1]=0.25
+
+        matrix[i[0]][i[1]]=count/4
+
+        print(matrix)
+        print("----")
+        matrices.append(matrix)
+    return matrices
+
+        
 if __name__ == "__main__":
 
     mapa=create_map()
-    sequence=init_secuence(1)
-    print(mapa)
+    emision_test=init_secuence(1)
+    prob_emision=emision(mapa,emision_test)
+    states_prob=states_prob(mapa)
+    states=init_states(mapa)
+    trans=trans_matrix(mapa,states)
+    #print(trans[0])
+
     
     
 
